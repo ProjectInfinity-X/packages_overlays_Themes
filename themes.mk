5d780c9a55389b635512b1bdc7d6c2acc5001464
tejas101k <tejassingh649@rediffmail.com>
2024-07-18 07:54:08 +0000

    Themes: Import  UDFPS icons & animations [3/3]
    Signed-off-by: tejas101k <tejassingh649@rediffmail.com>
    
    Co-authored-by: Davic Becker <DavicBecker@users.noreply.github.com>
    Co-authored-by: Aditya Choudhury <adityachoudhury0230@gmail.com>
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Co-authored-by: xpirasians <xpirasians@users.noreply.github.com>
    Signed-off-by: Tejas Singh <tejassingh649@rediffmail.com>

--- themes.mk
+++ themes.mk
@@ -472,3 +472,10 @@ PRODUCT_PACKAGES += \
     VolumeOutline \
     VolumeShadedLayer \
     VolumeAOSPRevamped
+
+# UDFPS    
+ifeq ($(TARGET_HAS_UDFPS),true)
+PRODUCT_PACKAGES += \
+    UdfpsIcons \
+    UdfpsAnimations
+endif
