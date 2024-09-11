54e7b5984c2e3a439418afb2b9f038cf30f54b9a
drkphnx <dark.phnx12@gmail.com>
2024-09-11 19:05:53 +0000

    Themes: Add Volte Round Font

--- themes.mk
+++ themes.mk
@@ -89,6 +89,7 @@ PRODUCT_PACKAGES += \
     FontStoropiaOverlay \
     FontSurferOverlay \
     FontUbuntuOverlay \
+    FontVolteOverlay \
     FontCircularStdOverlay \
     FontBlazmaHyperwaveOverlay \
     FontCardelinaOverlay \
